22ac85c88c552215c70f86eb8923bbbbd9e6d163
Sourav Sahoo <jackysourav1994@gmail.com>
2024-02-04 22:54:58 +0530

    Conversion of variable

---
+++ 01-basics/conversion.js
@@ -0,0 +1,15 @@
+let score = "ava"
+
+console.log(typeof score);
+
+let converted = Number(score)
+console.log(typeof converted);
+console.log(converted);
+
+/**
+  "33" -> 33
+  "s" -> NaN
+  false -> 0
+  null -> 0
+  undefined -> NaN
+ */
