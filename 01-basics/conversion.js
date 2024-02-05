4ab5317282f68b4212673a0e870d9bb113700508
Sourav Sahoo <jackysourav1994@gmail.com>
2024-02-05 09:13:34 +0530

    conversions and comparisions

--- 01-basics/conversion.js
+++ 01-basics/conversion.js
@@ -1,10 +1,10 @@
 let score = "ava"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let converted = Number(score)
-console.log(typeof converted);
-console.log(converted);
+// console.log(typeof converted);
+// console.log(converted);
 
 /**
   "33" -> 33
@@ -13,3 +13,18 @@ console.log(converted);
   null -> 0
   undefined -> NaN
  */
+
+let isTrue = false
+let s = String(isTrue)
+// console.log(s);
+// console.log(typeof s);
+
+
+let bool = +true
+// console.log(bool);
+
+// console.log(null >= 0); // true
+// console.log(null == 0); // false
+
+console.log(NaN == 0); // false
+console.log(NaN >= 0); // false
