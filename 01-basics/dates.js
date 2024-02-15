c042dbf1c97eb23f9fbe92f1d18c07d40ba8152b
Sourav Sahoo <jackysourav1994@gmail.com>
2024-02-15 19:54:54 +0530

    date formatting

--- 01-basics/dates.js
+++ 01-basics/dates.js
@@ -1,8 +1,11 @@
 let myDate = new Date()
 
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toISOString());
-console.log(myDate.toTimeString());
-console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+
+let newDate = new Date(2024,3,20,30,12);
+console.log(newDate.toLocaleString());
