264c37230acf73daaa283d8a79f4112b6f07acfa
Sourav Sahoo <jackysourav1994@gmail.com>
2024-02-15 19:48:34 +0530

    date file

---
+++ 01-basics/dates.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleTimeString());
